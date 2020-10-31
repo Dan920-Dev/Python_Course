05bf259d1e49c29760bed1b4455e433b3ced416d
Daniel Escobar <54414879+Danii920@users.noreply.github.com>
2020-10-30 23:20:17 -0600

    Add Exercise 02

---
+++ Exercises/Ex02.py
@@ -0,0 +1,14 @@
+# Convert from dollar to colón
+
+"""
+The colón has been a monetary unit of El Salvador since 1892. 
+It was progressively replaced by the US dollar in 2001. 
+The colón was issued since 1934 by the Central Reserve Bank of El Salvador
+"""
+
+print("Enter the amount in dollars you want to convert to colones: ")
+convertDollar = float(input())
+
+result = convertDollar * 8.75
+
+print(convertDollar, " Dollars in colones is: ", result)
--- README.md
+++ README.md
@@ -14,3 +14,4 @@
 
  ## Exercises
  - [Exercise 01](Exercises/Ex01.py): Program that calculates the area of ​​a triangle.
+ - [Exercise 02](Exercises/Ex02.py): Convert from dollar to colón.
