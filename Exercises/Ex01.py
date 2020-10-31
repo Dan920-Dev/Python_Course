d56586a78b289d7692498e8b437a94eaec8ee82a
Daniel Escobar <54414879+Danii920@users.noreply.github.com>
2020-10-30 21:34:46 -0600

    Add Exercise 01

---
+++ Exercises/Ex01.py
@@ -0,0 +1,11 @@
+# Program that calculates the area of ​​a triangle
+
+print("Hello, I will calculate the area of ​​a triangle\n")
+print("Type the base of the triangle: ")
+base = float(input())
+print("Type the heigth of the triangle: ")
+heigth = float(input())
+
+result = (base * heigth) / 2.0
+
+print("the area is: ",result)
--- README.md
+++ README.md
@@ -7,3 +7,6 @@
  - [Example 04](Examples/Example_04/main.py): Operators.
  - [Example 05](Examples/Example_05/main.py): keyboard input.
  - [Example 06](Examples/Example_06/main.py): Lists.
+
+ ## Exercises
+ - [Exercise 01](Exercises/Ex01.py): Program that calculates the area of ​​a triangle.
